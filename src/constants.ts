a99869811754623ada5172952d8ef245f4863b7c
Teodor Zafirov <36457245+t3dkich@users.noreply.github.com>
2022-11-11 17:28:06 +0200

    feat: maistestsubnet

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x21652b2b16DeB89a5FF8684A203c4e1E30ccfb30'
+
+export const INIT_CODE_HASH = '0xef94e1d988f92979973e4a736b0955c80a18a0046d50c78615a236a284387128'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
